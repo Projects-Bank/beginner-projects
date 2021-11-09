d5c8816bf598a7b214e943cacba631240719eeb2
cesar1605 <cesarthierry684@gmail.com>
2021-11-09 19:48:09 +0000

    exercise 1 completed

--- madlib_exercise_1.py
+++ madlib_exercise_1.py
@@ -1,7 +1,13 @@
-# Create a madlib
-
+# Create a madlib 
+ 
 # Write your variables here
-
+verb1 = input("give me a verb!") 
+place = input("choose a place") 
 # Write your madlib here
 
+madlib = f"I {verb1} to go to the {place}"
+
 # Write your print here
+
+print(madlib)
+
